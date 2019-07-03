5d3f78ae90be082cdae7063fdaa6c4e1315662f7
Yi Sun <yi.sun@intel.com>
2019-07-03 22:59:43 +0800

    Create test case for SFENCE
    Signed-off-by: Yi Sun <mail@sunyi.info>

--- guest/x86/memory_order.c
+++ guest/x86/memory_order.c
@@ -18,7 +18,7 @@
 
 #define USE_CPU_FENCE  0
 #define MAX_RUNNING_CPU 3
-#define NOP()       do { asm volatile ("nop\n\t" :::"memory"); } while(0)
+#define NOP()       do { asm volatile ("nop\n\t" :::); } while(0)
 
 void ap_main();
 void test1();
@@ -27,10 +27,13 @@ void (*test_cases[MAX_RUNNING_CPU])() = { NULL, test1, test2 };
 
 atomic_t begin_sem1;
 atomic_t begin_sem2;
-atomic_t end_sem;
+atomic_t end_sem1;
+atomic_t end_sem2;
 
 int X, Y;
+int ARRAY[1030];
 int r1, r2;
+int r3, r4;
 int id;
 
 int logical_processor_arbitration() {
@@ -53,27 +56,33 @@ int main(int ac, char **av)
 	int detected = 0;
 	atomic_set(&begin_sem1, 0);
 	atomic_set(&begin_sem2, 0);
-	atomic_set(&end_sem, 0);
+	atomic_set(&end_sem1, 0);
+	atomic_set(&end_sem2, 0);
 	setup_idt();
 
 	id = 0;
 
-	for (int i = 1; ; ++i) {
+	for (int i = 1;  ; ++i) {
 		X = Y = 0;
-		r1 = r2 = 1;
+		r1 = r2 = 0;
 
 		atomic_inc(&begin_sem1);
 		atomic_inc(&begin_sem2);
 
-		while(atomic_read(&end_sem) != 2) NOP();
-		atomic_set(&end_sem, 0);
+		while(atomic_read(&end_sem1) != 1 || atomic_read(&end_sem2) != 1) NOP();
+		atomic_set(&end_sem1, 0);
+		atomic_set(&end_sem2, 0);
 
-		if (r1 == 0 && r2 == 0) {
+		if (ARRAY[1029] != 1029) {
 			detected++;
 			printf("%d reorders detected after %d iterations\n", detected, i);
 		}
 
 		if( i % 10000 == 0) printf("BSP: times %d\n", i);
+
+		memset(ARRAY, 0, 1030);
+		//make sure ARRAY is clear.
+		asm volatile("mfence":::"memory");
 	}
 
 	ret = report_summary();
@@ -94,41 +103,54 @@ void ap_main() {
 	while(1)
 		test_cases[local_id]();
 }
-volatile long _x, _y;
-volatile long _rx, _ry;
 
 void test1() {
 	while(atomic_read(&begin_sem1) != 1) NOP();
 	atomic_dec(&begin_sem1);
 
+/* create a array by MOVNTI instruction:
+    ARRAY[0] = 0;
+    ARRAY[1] = 1;
+    ...
+    ARRAY[1022] = 1022;
+    ARRAY[1023] = 1023;
+*/
 	asm volatile(
-			"xor %0, %0\n\t                 "
-			"movl $1, %1\n\t                "
+			"movl $0, %0			\n\t"
+			"movl $1029, %1			\n\t"
+			"begin: 			\n\t"
+			"    lea ARRAY(,%0,4), %2	\n\t"
+			"    movntil  %0, (%2) 		\n\t"
+			"    inc   %0			\n\t"
+			"    cmp   %1, %0		\n\t"
+			"    jle   begin		\n\t"
 #if USE_CPU_FENCE
-			"mfence\n\t                  "
+			"sfence\n\t                  "
 #endif
-			"movl %2, %0\n\t                "
-			: "=r"(r1), "=m" (X)
-			: "m"(Y)
-			: "memory");
+			:
+			: "r"(r1), "r"(r2), "r"(r3)
+			: 
+			);
 
-	atomic_inc(&end_sem);
+	atomic_inc(&end_sem1);
+	pause();
 }
 
 void test2() {
-	while(atomic_read(&begin_sem2) != 1) NOP();
+	//Sync with test1, right after test1.
+	while(atomic_read(&begin_sem2) != 1 || atomic_read(&end_sem1) != 1) NOP();
 	atomic_dec(&begin_sem2);
 
 	asm volatile(
-			"xor %0, %0\n\t                 "
-			"movl $1, %1\n\t                "
-#if USE_CPU_FENCE
-			"mfence\n\t                  "
-#endif
-			"movl %2, %0\n\t                "
-			: "=r"(r2), "=m" (Y)
-			: "m"(X)
-			: "memory");
+			"movl %1, %0\n\t                 "
+			: "=r"(r4)
+			: "m"(ARRAY[1029])
+			: );
 
-	atomic_inc(&end_sem);
+	//Test memory re-ordering!!
+	if( r4 != 1029)
+		printf("ARRAY is reordered! (%d)\n", r4);
+
+	atomic_inc(&end_sem2);
 }
+
